ac4276e76debc2e93dc2357f59ddccfbd626e710
realgrm <gabriel.reis.mattoso@gmail.com>
2020-12-29 14:18:01 -0300

    Update Fedora-Workstation-post-install.sh

--- Fedora-Workstation-post-install.sh
+++ Fedora-Workstation-post-install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Set dark theme
 gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita-dark'
 
 # ------------ distro apps ------------
@@ -238,12 +239,19 @@ FOLDERS=(
 	~/.var/app/AppImages
 )
 
-
 for folder in ${FOLDERS[@]}; do
 	find $folder -type f -exec /bin/sh -c "file {} | grep -q executable && chmod +x {}" \;
 done
 
 
+# ------------ Miscelanious ------------
+
+# make avaliable libreoffice templates from Nautilus's new file menu
+mkdir -p $HOME/Templates/0-Libreoffice/
+
+ln -s \
+/var/lib/flatpak/app/org.libreoffice.LibreOffice/current/active/files/libreoffice/share/template/common \
+$HOME/Templates/0-Libreoffice/Templates
 
 # ------------ Manual configuration ------------
 
