57ccf905e1ce515f8d9a85a53847420b4cbd8543
realgrm <gabriel.reis.mattoso@gmail.com>
2020-12-29 13:41:29 -0300

    Update disk-usage-cli.sh

--- disk-usage-cli.sh
+++ disk-usage-cli.sh
@@ -1,5 +1,11 @@
 #!/bin/env bash
 
-printf "du -h -d 1 \| sort -h \n"
+printf "\ncurrent folder:"
+
 pwd 
-du -h -d 1 | sort -h
+
+printf "command executed: du -h -d 1 \| sort -h \n\nRunning without sudo will skip files without permission \nBy design errors are hidden \nPlease wait, this may take a while. \n\n"
+
+du -h -d 1 2>/dev/null | sort -h 
+
+printf "\ncd to a subfolder and rerun script \n\n"
