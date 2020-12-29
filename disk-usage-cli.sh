c560f2a2dcd71c01860fd56b775e5711b29fea76
realgrm <gabriel.reis.mattoso@gmail.com>
2020-12-29 15:05:36 -0300

    Update disk-usage-cli.sh

--- disk-usage-cli.sh
+++ disk-usage-cli.sh
@@ -5,12 +5,18 @@ printf "\ncurrent folder:"
 pwd 
 
 printf "command executed: du -h -d 1 \| sort -h
-
-Running without sudo will skip files without permission
-By design errors are hidden
-Please wait, this may take a while.
-
 "
+echo "
+By design errors are hidden
+Please wait, this may take a while."
+
+if [ "$EUID" != 0 ]; 
+then 
+echo "
+Running without sudo privileges will skip files that the user doesn't have permission to read
+"
+else echo ""
+fi
 
 du -h -d 1 2>/dev/null | sort -h 
 
